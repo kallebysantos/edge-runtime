5b77113e7efe876125120a6ed09b0685ebd3081c
kallebysantos <kalleby_santos@hotmail.com>
2024-05-22 17:02:42 +0100

    feat: Running custom model
    - Replication `Session` as `Pipeline`, support to run custom installed
    models (Embedding only)

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 # --features cli/tracing
-GIT_V_TAG=0.1.1 cargo build && EDGE_RUNTIME_PORT=9998 RUST_BACKTRACE=full ./target/debug/edge-runtime "$@" start \
-    --main-service ./examples/main \
-    --event-worker ./examples/event-manager
+ORT_DYLIB_PATH='./libonnxruntime.so' GIT_V_TAG=0.1.1 cargo build && SB_AI_MODELS_DIR='./temp/models/' EDGE_RUNTIME_PORT=9998 RUST_BACKTRACE=full ./target/debug/edge-runtime "$@" start \
+	--main-service ./examples/main \
+	--event-worker ./examples/event-manager
