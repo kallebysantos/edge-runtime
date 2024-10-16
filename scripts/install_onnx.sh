6779bee4ca9a06b9e1db6aae3b3e874077f55972
kallebysantos <kalleby_santos@hotmail.com>
2024-10-16 11:51:20 +0100

    stamp(sb_ai): upgrade ort version
    - Upgrading ort to `v1.19.2`, it fixex the `"Unsupported model IR
    version: 10, max supported IR version: 9"` error.

--- scripts/install_onnx.sh
+++ scripts/install_onnx.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-ONNX_VERSION=${1:-1.17.3}
+ONNX_VERSION=${1:-1.19.2}
 TARGETPLATFORM=$2
 SAVE_PATH=${3:-"./onnx-runtime"}
 
@@ -8,9 +8,9 @@ ONNX_DOWNLOAD_FILE="onnxruntime-linux"
 ONNX_TARGET_PLATFORM=$([ "$TARGETPLATFORM" == "linux/arm64" ] && echo "aarch64" || echo "x64")
 
 if [[ $* == *"--gpu"* ]]; then
-	ONNX_DOWNLOAD_FILE="$ONNX_DOWNLOAD_FILE-$ONNX_TARGET_PLATFORM-gpu-$ONNX_VERSION"
+  ONNX_DOWNLOAD_FILE="$ONNX_DOWNLOAD_FILE-$ONNX_TARGET_PLATFORM-gpu-$ONNX_VERSION"
 else
-	ONNX_DOWNLOAD_FILE="$ONNX_DOWNLOAD_FILE-$ONNX_TARGET_PLATFORM-$ONNX_VERSION"
+  ONNX_DOWNLOAD_FILE="$ONNX_DOWNLOAD_FILE-$ONNX_TARGET_PLATFORM-$ONNX_VERSION"
 fi
 
 wget -qO- "https://github.com/microsoft/onnxruntime/releases/download/v${ONNX_VERSION}/${ONNX_DOWNLOAD_FILE}.tgz" | tar zxv
